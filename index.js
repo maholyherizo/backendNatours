1fa2a281e9497482ccceb9d5adbd940112bbffac
xchess64 <ajocelynpatrick@gmail.com>
2019-11-06 07:21:19 +0100

    Intro: solution - importation d'un module et lecture fichier

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const hello = "Hello";
-console.log(hello);
+ // importer le module file system
+ const fs = require('fs');
+ // lire un fichier et mettre son résultat dans une var textIn
+ const textIn = fs.readFileSync('./1-node-farm/starter/txt/input.txt', 'utf-8');
+ // afficher le contenu de la variable
+ console.log(textIn);
